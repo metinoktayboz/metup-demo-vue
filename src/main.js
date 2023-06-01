ba27ea595424ab019b9bdf886516a22b42399b3c
moboz <metinoktayboz@gmail.com>
2023-06-01 14:34:45 +0300

    First Commit

---
+++ src/main.js
@@ -0,0 +1,34 @@
+import Vue from 'vue'
+import Vuetify from 'vuetify'
+import App from './App'
+import firebase from 'firebase'
+import router from './router'
+import { store } from './store'
+import DateFilter from './filters/date'
+import AlertCmp from './components/Shared/Alert.vue'
+
+Vue.use(Vuetify)
+Vue.config.productionTip = false
+
+Vue.filter('date', DateFilter)
+Vue.component('app-alert', AlertCmp)
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  store,
+  render: h => h(App),
+  created () {
+    firebase.initializeApp({
+      /*
+      implement your firebase user
+      */
+    })
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.$store.dispatch('autoSignIn', user)
+      }
+    })
+    this.$store.dispatch('loadMeetups')
+  }
+})
